804744f42871ce27df4234beacfebe0ec983ea60
pablocollova <pablocollova@gmail.com>
2024-09-30 11:56:06 +0200

    plantilla

---
+++ menu.php
@@ -0,0 +1,35 @@
+
+<nav class=" navbar-expand-lg navbar-light sticky-top py-0 pe-5 navbar">
+    <a href="index.html" class="navbar-brand ps-5 me-0">
+      <img style="height: 100%; z-index: 1001;" src="img/logo.png" alt="Logo">
+    </a>
+    <div id="buttonIdioma" class="button">
+      <div class="circle" title="Toggle Language"></div>
+    </div>
+
+    <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse vid-emp navbar-collapse  " id="navbarCollapse">
+
+
+
+
+      <div class="navbar-nav ms-auto p-4 p-lg-0" id="mainNav">
+
+        <a href="about.html" class="nav-item nav-link" data-lang="sobreNosotros"></a>
+        <div class="nav-item dropdown">
+          <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" data-lang="Servicios"></a>
+          <div class="dropdown-menu bg-light m-0">
+            <a href="project.html" class="dropdown-item" data-lang="luz y gas"></a>
+            <a href="feature.html" class="dropdown-item" data-lang="TelefoniaMovil"></a>
+            <a href="team.html" class="dropdown-item" data-lang="Telecomunicaciones y Alarmas"></a>
+            <a href="testimonial.html" class="dropdown-item" data-lang="PanelesSolares"></a>
+          </div>
+        </div>
+        <a href="contact.html" class="nav-item nav-link" data-lang="Contacto"></a>
+        <a href="https://seycom.nodogestion.com/login" class="nav-item nav-link active">Zona Distribuidor</a>
+      </div>
+    </div>
+  </nav>
